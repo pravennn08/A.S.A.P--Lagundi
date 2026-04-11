9417a226d3d03e30d132d3b89b8d79dda474f21f
Raven Magbanua <magbanuaraven08@gmail.com>
2026-04-11 14:11:46 +0800

    Prod

--- server/server.js
+++ server/server.js
@@ -49,7 +49,7 @@ app.use("/api/schedule", scheduleRouter);
 app.use("/api", userRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client", "dist")));
+  app.use(express.static(path.join(__dirname, "../client", "dist")));
 
   app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, "../client/", "dist", "index.html"));
